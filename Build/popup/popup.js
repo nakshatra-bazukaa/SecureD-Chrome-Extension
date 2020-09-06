5e5e526f8fe16f0fe107320b2a40780cbc328da1
AM-SAPP <8923499556nikhil@gmail.com>
2020-09-06 11:37:23 +0530

    popup js - 2

--- Build/popup/popup.js
+++ Build/popup/popup.js
@@ -5,6 +5,8 @@ const passGen = document.querySelector('#passGen');
 const generate = document.querySelector("#generate");
 const hidden = document.querySelector(".hidden");
 const copy = document.querySelector("#copyit");
+const save_password = document.querySelector("#save");
+
 
 check.addEventListener('click', e => {
     custom.classList.toggle('d-none');
@@ -27,3 +29,33 @@ slider.oninput = function () {
     output.innerHTML = this.value;
 }
 
+// custum password options
+
+const upper = document.querySelector("input[name=uppercase]");
+const lower = document.querySelector("input[name=lowercase]");
+const number = document.querySelector("input[name=number]");
+const special_char = document.querySelector("input[name=special_char]");
+
+
+upper.addEventListener('change', e => sessionStorage.setItem("xs", e.target.checked));
+
+lower.addEventListener('change', e => sessionStorage.setItem("ys", e.target.checked));
+
+number.addEventListener('change', e => sessionStorage.setItem("zs", e.target.checked));
+
+special_char.addEventListener('change', e => sessionStorage.setItem("ts", e.target.checked));
+
+// copy password
+
+
+copy.addEventListener('click', () => {
+
+    /* Select the text field */
+    passGen.select();
+    passGen.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+    copy.innerHTML = "COPIED";
+  });
+  
